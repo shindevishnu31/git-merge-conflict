129eac9c667cf96f6228833c394fe1a941d88bb5
Akshay <akshayshendkar1518@gmail.com>
2022-09-22 11:50:50 +0530

    Addition api added

---
+++ src/main/java/com/bnt/controller/AdditionController.java
@@ -0,0 +1,20 @@
+package com.bnt.controller;
+
+import javax.websocket.server.PathParam;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("git-merge/")
+public class AdditionController {
+
+	@GetMapping("add")
+	public int add(@PathParam("num1") int a,@PathParam("num2") int b){
+		
+		
+		return a+b;
+		
+	}
+}
